10a55636f28df0f200d760c912eaeb7bcc42cb52
lzd <lzd@doooer.com>
2015-08-18 20:55:49 +0800

    modify FileUtil add copyFile

--- src/com/soo/ify/util/FileUtils.java
+++ src/com/soo/ify/util/FileUtils.java
@@ -16,6 +16,8 @@
 package com.soo.ify.util;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import android.os.StatFs;
@@ -117,4 +119,88 @@ public class FileUtils {
         final StatFs stats = new StatFs(path);
         return (long) stats.getBlockSize() * (long) stats.getAvailableBlocks();
     }
+    
+    
+//    /**
+//     * 根据目录与文件名创建一个文件，注意：flag为true总是返回一个全新的文件，flag为false
+//     * 表示如果没有该文件就创建一个新的，有就直接返回
+//     */
+//    public static File createFile(String filePath, boolean flag){
+//        File file = new File(filePath);
+//        String directory = file.getParent();
+//        String name = file.getName();
+//        return createFile(directory, name, flag);
+//    }
+//
+//    /**
+//     * 根据目录与文件名创建一个文件，注意：flag为true总是返回一个全新的文件，flag为false
+//     * 表示如果没有该文件就创建一个新的，有就直接返回
+//     */
+//    public static File createFile(String directory, String name, boolean flag){
+//        File file = null;
+//        if(!DevUtils.sdcardAvailable()) return file;
+//        if(directory != null && directory.trim().length() > 0
+//                && name != null && name.trim().length() > 0){
+//            File dirFile = new File(directory);
+//            dirFile.mkdirs();
+//            file = new File(dirFile, name);
+//            if(flag) file.delete();
+//            try {
+//                file.createNewFile();
+//            } catch (IOException e1) {
+//                e1.printStackTrace();
+//            }
+//        }
+//        return file;
+//    }
+    
+    
+    /**
+     * 复制单个文件    拍照选择图片用到
+     * @param oldPath 旧文件路径
+     * @param newPath 新路径
+     * @param isForceOverride 如果新路径存在同名文件，是否强制覆盖新文件。
+     * @return 1成功, -1失败, 0新路径存在同名文件，跳过。
+     */
+    public static int copyFile(String oldPath, String newPath, boolean isForceOverride) {
+        int r = 1;
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        try {
+            int len = 0;
+            File oldfile = new File( oldPath );
+            File newfile = new File( newPath );
+            
+            // 要复制的文件存在
+            if (oldfile.exists()) {
+                // 如果新文件不存在, 或存在，并强制覆盖
+                if(!newfile.exists() || (newfile.exists() && isForceOverride)){
+                    fis = new FileInputStream( oldPath );
+                    fos = new FileOutputStream( newPath );
+                    byte[] buffer = new byte[1444];
+                    while ((len = fis.read( buffer )) != -1) {
+                        fos.write( buffer, 0, len );
+                    }
+                    fos.flush();
+                } else r = 0;
+                
+            } else r = -1;
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+            r = -1;
+        } finally {
+            try {
+                if (fos != null)
+                    fos.close();
+                if (fis != null)
+                    fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return r;
+
+    }
+
 }
